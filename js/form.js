f0775c3ddba28330f212305523d9f46015425497
Sergey <88794938+alfabouh@users.noreply.github.com>
2023-10-09 10:02:17 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,91 +1,300 @@
-var but1;
-var field1;
-var items1;
-var message;
+var items2;
+var field2;
+var but2;
+
+var it_input;
+var form_message;
+var form_input;
+var prop_input;
+var add_input;
+
+var constructors = [];
+const DEFAULT = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-    var q_all = document.querySelectorAll("#items1 option");
-    but1 = document.getElementById("button_ac1");
-    field1 = document.getElementById("field1");
-    items1 = document.getElementById("items1");
+    it_input = document.createElement("input");
+    form_message = document.createElement("div");
+    form_message.id = "fill_2";
+    form_message.className ="result my-1 px-1";
+    setConstructors();
 
-    q_all.forEach(function (e) {
-        e.textContent += " - " + e.value + "$";
+    but2 = document.getElementById("button_ac2");
+    field2 = document.getElementById("field2");
+    items2 = document.getElementById("items2");
+
+    checkSelectionAction(items2.value);
+
+    but2.addEventListener("click", function() {
+        location.reload(true);
     });
 
-    but1.addEventListener("click", action1);
+    items2.addEventListener("change", function() {
+        var v2 = items2.value;
+        it_input.value = "";
+        checkSelectionAction(v2);
+    });
 });
 
-function setMessage(value, value2) {
-    var anchor1 = document.getElementById("anchor_1");
-    var check = document.getElementById("fill_1");
-    var l1 = (
-        value2 === true
-        ? "success"
-        : "warn"
-    );
-    message = document.createElement("div");
-    message.id = "fill_1";
+function setConstructors() {
+    var cons0;
+    var cons1;
+    var cons2;
+    var rad1_c = document.createElement("div");
+    var inp1 = document.createElement("input");
+    var lab1 = document.createElement("label");
+    var rad2_c = document.createElement("div");
+    var inp2 = document.createElement("input");
+    var lab2 = document.createElement("label");
+    var rad3_c = document.createElement("div");
+    var inp3 = document.createElement("input");
+    var lab3 = document.createElement("label");
+    var check1_c = document.createElement("div");
+    var inp4 = document.createElement("input");
+    var lab4 = document.createElement("label");
+    var inp5 = document.createElement("input");
+    var check2_c = document.createElement("div");
+    var lab5 = document.createElement("label");
 
-    if (value !== null) {
-        message.className = l1 + " my-1 px-1";
-        message.innerHTML = "<h5>" + value + "</h5>";
-    } else {
-        if (anchor1.contains(check)) {
-            anchor1.removeChild(check);
+    cons0 = document.createElement("div");
+    cons0.id = "ANC";
+    cons0.className = "no_val";
+    cons0.innerHTML = "<Цена диагностики 1 устройства: 1$>";
+
+
+    cons1 = document.createElement("div");
+    cons1.id = "ANC";
+    cons1.className = "row row-cols-auto my-1";
+
+    //+++++
+    rad1_c.className = "col-12";
+
+    inp1.type = "radio";
+    inp1.name = "option_1";
+    inp1.id = "option1";
+    inp1.value = "49";
+    inp1.checked = true;
+
+    lab1.setAttribute("for", "option1");
+    lab1.textContent = ("Замена" + " - " + inp1.value + "$");
+
+    rad1_c.appendChild(inp1);
+    rad1_c.appendChild(lab1);
+    //+++++
+
+    //+++++
+    rad2_c.className = "col-12";
+
+    inp2.type = "radio";
+    inp2.name = "option_1";
+    inp2.id = "option2";
+    inp2.value = "69";
+
+    lab2.setAttribute("for", "option2");
+    lab2.textContent = ("Ремонт" + " - " + inp2.value + "$");
+    //+++++
+
+    //+++++
+    rad3_c.className = "col-12";
+
+    inp3.type = "radio";
+    inp3.name = "option_1";
+    inp3.id = "option3";
+    inp3.value = "99";
+
+    lab3.setAttribute("for", "option3");
+    lab3.textContent = ("Обновление" + " - " + inp3.value + "$");
+    //+++++
+
+    rad1_c.appendChild(inp1);
+    rad1_c.appendChild(lab1);
+    rad2_c.appendChild(inp2);
+    rad2_c.appendChild(lab2);
+    rad3_c.appendChild(inp3);
+    rad3_c.appendChild(lab3);
+
+    cons1.appendChild(rad1_c);
+    cons1.appendChild(rad2_c);
+    cons1.appendChild(rad3_c);
+
+    cons2 = document.createElement("div");
+    cons2.id = "ANC";
+    cons2.className = "row row-cols-auto my-1";
+
+    check1_c.className = "col-12";
+
+    inp4.type = "checkbox";
+    inp4.name = "check_1";
+    inp4.id = "check1";
+    inp4.value = "19";
+
+    lab4.htmlFor = "check1";
+    lab4.textContent = ("SSD 512 GB" + "(+" + inp4.value + "$)");
+
+    check2_c.className = "col-12";
+
+    inp5.type = "checkbox";
+    inp5.name = "check_1";
+    inp5.id = "check2";
+    inp5.value = "39";
+
+    lab5.htmlFor = "check2";
+    lab5.textContent = ("OLED Display" + "(+" + inp5.value + "$)");
+
+    check1_c.appendChild(inp4);
+    check1_c.appendChild(lab4);
+    check2_c.appendChild(inp5);
+    check2_c.appendChild(lab5);
+
+    cons2.appendChild(check1_c);
+    cons2.appendChild(check2_c);
+
+    constructors.push(cons0);
+    constructors.push(cons1);
+    constructors.push(cons2);
+
+    cons1.querySelectorAll("input[type=\"radio\"][name=\"option_1\"]")
+        .forEach(function(e) {
+        e.addEventListener("change", function() {
+            checkRadioAction(e);
+        });
+    });
+
+    cons2.querySelectorAll("input[type=\"checkbox\"][name=\"check_1\"]")
+        .forEach(function(e) {
+        e.addEventListener("change", function() {
+            var c1 = 0;
+            cons2.querySelectorAll("input[type=\"checkbox\"][name=\"check_1\"]")
+                .forEach(function(e) {
+                if (e.checked) {
+                    c1 += parseInt(e.value);
+                }
+            });
+            add_input = c1;
+            setMesInt(form_input, prop_input, add_input);
+        });
+    });
+}
+
+function onSelectionClick(sel_opt) {
+    var anchor1 = document.getElementById("anchor_2");
+    var anchor2 = document.getElementById("prop_anchor_1");
+    var anchor3 = document.getElementById("prop_anchor_2");
+    var inp_check = document.getElementById("field2");
+    var fill_check = document.getElementById("fill_2");
+    var anc_1 = document.getElementById("ANC");
+    var a1 = constructors[sel_opt];
+
+    if (sel_opt >= 0 && sel_opt < constructors.length) {
+        anchor2.appendChild(a1);
+
+        it_input.type = "text";
+        it_input.id = "field2";
+        it_input.placeholder = "Количество устройств";
+
+        switch (parseInt(sel_opt)) {
+            case 2: {
+                form_input = DEFAULT;
+                a1.querySelectorAll
+                ("input[type=\"checkbox\"][name=\"check_1\"]")
+                    .forEach(function(e) {
+                    e.addEventListener("change", function() {
+                        var c1 = (
+                            e.checked
+                            ? 1
+                            : -1
+                        );
+                        add_input += e.value * c1;
+                    });
+                });
+                prop_input = 199;
+                break;
+            }
+            case 1: {
+                form_input = DEFAULT;
+                a1.querySelectorAll("input[type=\"radio\"][name=\"option_1\"]")
+                    .forEach(function(e) {
+                    checkRadioAction(e);
+                });
+                add_input = 0;
+                break;
+            }
+            default: {
+                form_input = DEFAULT;
+                add_input = 0;
+                prop_input = 1;
+            }
         }
-    }
 
-    if (anchor1.contains(check)) {
-        anchor1.replaceChild(message, check);
+        it_input.addEventListener("keydown", function (e) {
+            if ((["1","2","3","4","5","6","7","8","9","0"])
+                .indexOf(e.key) === -1) {
+                if (e.key !== "Backspace" && e.key !== "Delete") {
+                    e.preventDefault();
+                }
+            }
+        });
+
+        it_input.addEventListener("input", function(e) {
+            var v1 = it_input.value;
+            form_input = checkField(v1);
+            setMesInt(form_input, prop_input, add_input);
+        });
+
+        if (!anchor3.contains(inp_check)) {
+            anchor3.appendChild(it_input);
+        }
+
+        if (anchor2.contains(anc_1)) {
+            anchor2.removeChild(anc_1);
+        }
+
+        if (!anchor1.contains(fill_check)) {
+            anchor1.appendChild(form_message);
+        }
+
+        setMesInt(form_input, prop_input, add_input);
     } else {
-        anchor1.appendChild(message);
-    }
-}
-
-function calcAction1(value_it1, value_it2) {
-    var result1 = value_it1 * value_it2;
-    var rs = "Стоимость заказа: " + result1 + "$";
-    if (value_it1 === null) {
-        setMessage("Не выбран товар", false);
-        return;
-    }
-    setMessage(rs, true);
-}
-
-function getSelectedValue() {
-    var sel_o = items1.selectedIndex;
-    var c1 = (
-        (sel_o === -1)
-        ? null
-        : items1.options[sel_o].value
-    );
-    return c1;
-}
-
-function action1() {
-    var l1 = checkField(field1.value);
-    if (l1 === -1) {
-        setMessage("Строка пуста", false);
-    } else if (l1 === 0) {
-        setMessage("Строка содержит неправильные символы", false);
-    } else {
-        calcAction1(getSelectedValue(), parseInt(field1.value));
+        if (anchor3.contains(inp_check)) {
+            anchor3.removeChild(inp_check);
+        }
+        if (anchor2.contains(anc_1)) {
+            anchor2.removeChild(anc_1);
+        }
+        if (anchor1.contains(fill_check)) {
+            anchor1.removeChild(fill_check);
+        }
     }
 }
 
 function checkField(field) {
-    var f1 = field.trim();
-    var regex = /^\d+$/;
-    var c2 = (
-        regex.test(f1) === true
-        ? 1
-        : 0
+    var inp = (
+        field !== ""
+        ? parseInt(field)
+        : DEFAULT
     );
-    var c1 = (
-        f1 === ""
-        ? -1
-        : c2
-    );
-    return c1;
+    return inp;
+}
+
+function setMesInt(form, prop, add) {
+    form_message.innerHTML = "<h5> Итоговая цена: " +
+        (parseInt(form) * (parseInt(add) + parseInt(prop))) + "$</h5>";
+}
+
+//============================================================
+
+function checkRadioAction(e) {
+    if (e.checked) {
+        onRadioClick(e);
+    }
+}
+
+function checkSelectionAction(sel_opt) {
+    onSelectionClick(sel_opt);
+}
+
+//============================================================
+
+function onRadioClick(butt) {
+    prop_input = butt.value;
+    setMesInt(form_input, prop_input, add_input);
 }
